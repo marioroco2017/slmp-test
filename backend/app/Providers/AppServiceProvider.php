38814623370d44cb5dcca1bee3affc39b7e054bc
mario <marioroco2017@gmail.com>
2026-03-29 16:16:06 +0800

    Update AppServiceProvider.php

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\LazyLoadingViolationException;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +28,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        Model::shouldBeStrict();
+        Model::handleLazyLoadingViolationUsing(function ($model, $relation) {
+            $class = get_class($model);
+            if (app()->isLocal()) {
+                throw new LazyLoadingViolationException($model, $relation);
+            }
+            info('Attempted to lazy load "' . $relation . '" on model "' . $class . '"');
+        });
+
+        DB::prohibitDestructiveCommands(app()->isProduction());
+
+        //USE IMMUTABLE DATES EXAMPLE: DATE::now() RETURNS A CARBONIMMUTABLE INSTANCE INSTEAD OF A CARBON INSTANCE
+        Date::use(CarbonImmutable::class);
+
+
+
     }
 }
